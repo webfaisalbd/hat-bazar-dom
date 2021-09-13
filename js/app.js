174497cd47cf37b2d1794d672436f94464c02b20
faisalcsebd <faisal.cse.bd@gmail.com>
2021-09-13 09:44:48 +0600

    total added

--- js/app.js
+++ js/app.js
@@ -30,7 +30,7 @@ const addToCart = (id, price) => {
   updatePrice("price", price);
 
   updateTaxAndCharge();
-
+  updateTotal();
   document.getElementById("total-Products").innerText = count;
 };
 
@@ -45,12 +45,12 @@ const updatePrice = (id, value) => {
   const convertedOldPrice = getInputValue(id);
   const convertPrice = parseFloat(value);
   const total = convertedOldPrice + convertPrice;
-  document.getElementById(id).innerText = Math.round(total);
+  document.getElementById(id).innerText = total;
 };
 
 // set innerText function
 const setInnerText = (id, value) => {
-  document.getElementById(id).innerText = Math.round(value);
+  document.getElementById(id).innerText = value;
 };
 
 // update delivery charge and total Tax
@@ -75,7 +75,6 @@ const updateTotal = () => {
   const grandTotal =
     getInputValue("price") + getInputValue("delivery-charge") +
     getInputValue("total-tax");
-  console.log(grandTotal);
   document.getElementById("total").innerText = grandTotal;
 
 };
